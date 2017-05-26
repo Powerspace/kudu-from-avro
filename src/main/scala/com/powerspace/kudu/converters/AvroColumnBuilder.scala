17fd7b612f38f9fabcda61f708129b939e6b11bb
Laurent VALDES <valderama@gmail.com>
2017-05-26 15:30:07 +0200

    Introcude default values + schema analysis object

--- src/main/scala/com/powerspace/kudu/converters/AvroColumnBuilder.scala
+++ src/main/scala/com/powerspace/kudu/converters/AvroColumnBuilder.scala
@@ -2,52 +2,46 @@ package com.powerspace.kudu.converters
 
 import org.apache.avro.Schema
 import org.apache.avro.Schema.Field
+import org.apache.kudu.ColumnSchema.ColumnSchemaBuilder
 import org.apache.kudu.{ColumnSchema, Type}
-import org.apache.kudu.ColumnSchema.{ColumnSchemaBuilder, CompressionAlgorithm}
 
-import collection.JavaConverters._
+import scala.annotation.tailrec
+import scala.collection.JavaConverters._
+import scala.collection.immutable.Seq
+import scala.collection.mutable
 
 case class AvroColumn(name: String, schema: Schema)
 
 object AvroColumnBuilder {
-  def apply(avroPath: String): AvroColumnBuilder = new AvroColumnBuilder(avroPath)
+  def apply(rawSchema: String): AvroColumnBuilder = new AvroColumnBuilder(new Schema.Parser().parse(rawSchema))
 
   def fieldToAvroColumn(field: Field) = AvroColumn(field.name(), field.schema())
 }
 
-class AvroColumnBuilder(avroSchema: String) extends ColumnBuilder {
+object SchemaAnalysis {
+  def toKuduColumn(field: Field): (String, ColumnSchemaBuilder) = (field.name(), recurseGeneration(field.name(), field.schema(), field.defaultVal()))
 
-  val schema: Schema = new Schema.Parser().parse(avroSchema)
-
-  override def baseColumns() = {
-    avroColumns().map(
-      field => (field.name, toKuduColumn(field.name, field.schema)))
-  }
-
-
-  def avroColumns(): List[AvroColumn] = {
-    schema.getFields.asScala.map(AvroColumnBuilder.fieldToAvroColumn).toList
-  }
-
-  private def toKuduColumn(name: String, avroSchema: org.apache.avro.Schema): ColumnSchemaBuilder = {
+  private def recurseGeneration(name: String, avroSchema: org.apache.avro.Schema, defaultValue: AnyRef): ColumnSchema.ColumnSchemaBuilder = {
     import org.apache.avro.Schema.Type._
 
+
+
     avroSchema.getType match {
-      case INT => new ColumnSchema.ColumnSchemaBuilder(name, Type.INT32)
-      case STRING => new ColumnSchema.ColumnSchemaBuilder(name, Type.STRING)
-      case BOOLEAN => new ColumnSchema.ColumnSchemaBuilder(name, Type.BOOL)
-      case BYTES => new ColumnSchema.ColumnSchemaBuilder(name, Type.BINARY)
-      case DOUBLE => new ColumnSchema.ColumnSchemaBuilder(name, Type.DOUBLE)
-      case FLOAT => new ColumnSchema.ColumnSchemaBuilder(name, Type.FLOAT)
-      case LONG => new ColumnSchema.ColumnSchemaBuilder(name, Type.INT64)
-      case FIXED => new ColumnSchema.ColumnSchemaBuilder(name, Type.BINARY)
-      case ENUM => new ColumnSchema.ColumnSchemaBuilder(name, Type.STRING)
+      case INT => new ColumnSchema.ColumnSchemaBuilder(name, Type.INT32).defaultValue(defaultValue)
+      case STRING => new ColumnSchema.ColumnSchemaBuilder(name, Type.STRING).defaultValue(defaultValue)
+      case BOOLEAN => new ColumnSchema.ColumnSchemaBuilder(name, Type.BOOL).defaultValue(defaultValue)
+      case BYTES => new ColumnSchema.ColumnSchemaBuilder(name, Type.BINARY).defaultValue(defaultValue)
+      case DOUBLE => new ColumnSchema.ColumnSchemaBuilder(name, Type.DOUBLE).defaultValue(defaultValue)
+      case FLOAT => new ColumnSchema.ColumnSchemaBuilder(name, Type.FLOAT).defaultValue(defaultValue)
+      case LONG => new ColumnSchema.ColumnSchemaBuilder(name, Type.INT64).defaultValue(defaultValue)
+      case FIXED => new ColumnSchema.ColumnSchemaBuilder(name, Type.BINARY).defaultValue(defaultValue)
+      case ENUM => new ColumnSchema.ColumnSchemaBuilder(name, Type.STRING).defaultValue(defaultValue)
       case UNION =>
         if (avroSchema.getTypes.asScala.exists(_.getType == NULL)) {
           // In case of a union with null, eliminate it and make a recursive call
           val remainingUnionTypes = avroSchema.getTypes.asScala.filterNot(_.getType == NULL)
           if (remainingUnionTypes.size == 1) {
-            toKuduColumn(name, remainingUnionTypes.head).nullable(true)
+            recurseGeneration(name, remainingUnionTypes.head, defaultValue).nullable(true)
           } else {
             throw new IllegalArgumentException(s"Unsupported union type ${avroSchema.getType}")
           }
@@ -59,5 +53,17 @@ class AvroColumnBuilder(avroSchema: String) extends ColumnBuilder {
     }
 
   }
+}
 
+
+class AvroColumnBuilder(schema: Schema) extends ColumnBuilder {
+  import SchemaAnalysis._
+
+  override def baseColumns() = {
+    fields().map(field => toKuduColumn(field))
+  }
+
+  def fields(): List[Field] = {
+    schema.getFields.asScala.toList
+  }
 }
--- src/test/scala/com/powerspace/kudu/converters/AvroColumnBuilderTest.scala
+++ src/test/scala/com/powerspace/kudu/converters/AvroColumnBuilderTest.scala
@@ -4,7 +4,6 @@ import org.apache.kudu.Type._
 import org.scalatest.{FlatSpec, Matchers}
 import org.apache.avro.Schema.{Type => AvroType}
 import org.apache.avro.SchemaParseException
-import org.apache.kudu.ColumnSchema.CompressionAlgorithm
 import org.apache.kudu.ColumnSchema.CompressionAlgorithm.LZ4
 
 class AvroColumnBuilderTest extends FlatSpec with Matchers {
@@ -39,8 +38,8 @@ class AvroColumnBuilderTest extends FlatSpec with Matchers {
     """.stripMargin
 
   "the Avro converter" should "properly parse an Avro schema" in {
-    val converter = new AvroColumnBuilder(schema)
-    val List(a, b, c) = converter.avroColumns()
+    val converter = AvroColumnBuilder(schema)
+    val List(a, b, c) = converter.fields()
     a.name should === ("valid")
     a.schema.getType should === (AvroType.BOOLEAN)
     b.name should === ("eventType")
@@ -50,7 +49,7 @@ class AvroColumnBuilderTest extends FlatSpec with Matchers {
   }
 
   it should "properly map to kudu columns" in {
-    val converter = new AvroColumnBuilder(schema)
+    val converter = AvroColumnBuilder(schema)
     val List(a, b, c) = converter.kuduColumns(LZ4, List())
     a.getName should === ("valid")
     a.getType should === (BOOL)
@@ -63,7 +62,7 @@ class AvroColumnBuilderTest extends FlatSpec with Matchers {
   }
 
   it should "crash if it found an unknown type" in {
-    an [SchemaParseException] should be thrownBy  new AvroColumnBuilder(unknownTypeSchema)
+    an [SchemaParseException] should be thrownBy AvroColumnBuilder(unknownTypeSchema)
   }
 
 }
